2f01a7f87c5c7dc6f2e3709cc28d53b400995af0
Jay D'Aiuto <93616573+jdaiuto29@users.noreply.github.com>
2022-02-08 22:55:58 -0500

    fixed api

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ document.addEventListener('DOMContentLoaded', function() {
         e.preventDefault();
         const searchString = document.querySelector('.search-bar').value
         const urlEncodedSearchString = encodeURIComponent(searchString);
-        fetch(`http://www.omdbapi.com/?apikey=59354c85&s=${urlEncodedSearchString}`)
+        fetch(`https://www.omdbapi.com/?apikey=59354c85&s=${urlEncodedSearchString}`)
             .then((res) => res.json())
             .then((data) => {
                 renderMovies(data.Search);
